01f86f87a7f9e2d33d1b925ebf56768157929291
hrithik-infinite <hrithikinfinite@gmail.com>
2022-08-24 15:04:23 +0530

    hello wrold file from laptop

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello WOrld!!!");
+		System.out.println("Hello WOrld2!!!");
+		System.out.println("Hello WOrld!3!!");
+		System.out.println("Hello WOrld!!gugyfuyfuggkh");
+	}
+}
